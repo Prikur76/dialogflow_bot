187c7ff683d703dd8d42c3e4ddcfe9f61ef6793a
Prikur76 <44672462+Prikur76@users.noreply.github.com>
2023-10-04 13:13:37 +0300

    Add DialogFlow to VK bot

--- vk_bot.py
+++ vk_bot.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import random
 
@@ -5,24 +6,45 @@ import vk_api as vk
 from dotenv import load_dotenv
 from vk_api.longpoll import VkLongPoll, VkEventType
 
+from dialog_flow import DialogFlow
+from error_handler import LogsHandler
+
 load_dotenv()
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO)
+
 
 def echo(event, vk_api):
     user_id = event.user_id
     user_message = event.text
-    print(f'{user_id}: {user_message}')
-    vk_api.messages.send(
-        user_id=event.user_id,
-        message=event.text,
-        random_id=random.randint(1,1000)
-    )
+    try:
+        flow = DialogFlow(os.getenv('PROJECT_ID'))
+        flow_answer = flow.detect_intent_text(
+            user_id, user_message)
+        if flow_answer:
+            vk_api.messages.send(
+                user_id=user_id,
+                message=flow_answer['answer'],
+                random_id=random.randint(1, 1000000000)
+            )
+    except Exception:
+        logging.exception('Проблема с получением/отправкой сообщений')
 
 
 if __name__ == "__main__":
-    vk_token = os.getenv('VK_ACCESS_TOKEN')
-    vk_session = vk.VkApi(token=vk_token)
-    vk_api = vk_session.get_api()
-    longpoll = VkLongPoll(vk_session)
-    for event in longpoll.listen():
-        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-            echo(event, vk_api)
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+    logger.addHandler(LogsHandler())
+    logger.info('VK бот запущен')
+
+    try:
+        vk_session = vk.VkApi(token=os.getenv('VK_ACCESS_TOKEN'))
+        vk_api = vk_session.get_api()
+        longpoll = VkLongPoll(vk_session)
+        for event in longpoll.listen():
+            if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+                echo(event, vk_api)
+    except Exception:
+        logger.exception('Возникла ошибка в VK боте')
