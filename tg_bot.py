cc2c408d6c6b0e04b5bd0c041ce796719432ce66
Prikur76 <44672462+Prikur76@users.noreply.github.com>
2023-10-04 21:13:47 +0300

    Fix tg_bot.py, vk_bot.py

--- tg_bot.py
+++ tg_bot.py
@@ -2,23 +2,21 @@ import logging
 import os
 
 from dotenv import load_dotenv
-from telegram import Update, ForceReply
+from telegram import Update, ForceReply, Bot
 from telegram.ext import (Updater, CommandHandler,
                           MessageHandler, Filters,
                           CallbackContext)
 
 from dialog_flow import DialogFlow
-from error_handler import LogsHandler
+from logshandler import TelegramLogsHandler
 
 load_dotenv()
 
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 def start(update: Update, context: CallbackContext) -> None:
-    """Send a message when the command /start is issued."""
+    """Отправляет привественное сообщение при команде /start"""
     user = update.effective_user
     update.message.reply_markdown_v2(
         fr'Здравствуй, {user.mention_markdown_v2()}\!',
@@ -26,7 +24,7 @@ def start(update: Update, context: CallbackContext) -> None:
 
 
 def help_command(update: Update, context: CallbackContext) -> None:
-    """Send a message when the command /help is issued."""
+    """Отправляет привественное сообщение при команде /help"""
     update.message.reply_text('Help!')
 
 
@@ -34,31 +32,46 @@ def echo_with_dialogflow(update: Update, context: CallbackContext) -> None:
     """Возвращает ответ DialogFlow."""
     session_id = update.effective_chat.id
     user_message = update.message.text
-    try:
-        flow = DialogFlow(os.getenv('PROJECT_ID'))
-        flow_answer = flow.detect_intent_text(
-            session_id, user_message)
+    flow = DialogFlow(os.environ.get('PROJECT_ID'))
+    flow_answer = flow.detect_intent_text(
+        session_id, user_message)
+    if flow_answer:
         update.message.reply_text(flow_answer['answer'])
-    except Exception:
-        logger.exception('Возникла проблема с DialogFlow')
+    return
 
 
 if __name__ == '__main__':
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.INFO)
-    logger.addHandler(LogsHandler())
-    logger.info('TG бот запущен')
+    admin_chat_id = os.environ.get('SERVICE_CHAT_ID')
+    admin_bot = Bot(token=os.environ.get('SERVICE_BOT_TOKEN'))
+    admin_bot_handler = TelegramLogsHandler(
+        admin_bot, admin_chat_id)
+    admin_bot_handler.setLevel(logging.DEBUG)
+    botformatter = logging.Formatter(
+        fmt='{message}', style='{')
+    admin_bot_handler.setFormatter(botformatter)
+    logger.addHandler(admin_bot_handler)
+
+    streamhandler = logging.StreamHandler()
+    streamhandler.setLevel(logging.ERROR)
+    streamformatter = logging.Formatter(
+        fmt='{asctime} - {levelname} - {name} - {message}',
+        style='{')
+    streamhandler.setFormatter(streamformatter)
+    logger.addHandler(streamhandler)
+    logger.debug('TG бот запущен')
 
     try:
-        updater = Updater(os.getenv('DF_BOT_TOKEN'))
+        updater = Updater(os.environ.get('DF_BOT_TOKEN'))
         dispatcher = updater.dispatcher
-        dispatcher.add_handler(CommandHandler("start", start))
-        dispatcher.add_handler(CommandHandler("help", help_command))
+        dispatcher.add_handler(CommandHandler('start', start))
+        dispatcher.add_handler(CommandHandler('help', help_command))
         dispatcher.add_handler(MessageHandler(
             Filters.text & ~Filters.command, echo_with_dialogflow)
         )
 
         updater.start_polling()
         updater.idle()
-    except Exception:
-        logger.exception('Возникла ошибка в TG боте')
+
+    except Exception as e:
+        logger.debug('Возникла ошибка в DialogFlow tg-боте')
+        logger.exception(e)
--- vk_bot.py
+++ vk_bot.py
@@ -5,46 +5,61 @@ import random
 import vk_api as vk
 from dotenv import load_dotenv
 from vk_api.longpoll import VkLongPoll, VkEventType
+from telegram import Bot
 
 from dialog_flow import DialogFlow
-from error_handler import LogsHandler
+from logshandler import TelegramLogsHandler
 
 load_dotenv()
 
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 def echo(event, vk_api):
     user_id = event.user_id
     user_message = event.text
+
+    flow = DialogFlow(os.environ.get('PROJECT_ID'))
+    flow_answer = flow.detect_intent_text(
+        user_id, user_message)
+
+    if not flow_answer['is_fallback']:
+        vk_api.messages.send(
+            user_id=user_id,
+            message=flow_answer['answer'],
+            random_id=random.randint(1, 1000000000)
+        )
+
+
+if __name__ == '__main__':
+    admin_chat_id = os.environ.get('SERVICE_CHAT_ID')
+    admin_bot = Bot(token=os.environ.get('SERVICE_BOT_TOKEN'))
+    admin_bot_handler = TelegramLogsHandler(
+        admin_bot, admin_chat_id)
+    admin_bot_handler.setLevel(logging.DEBUG)
+    botformatter = logging.Formatter(
+        fmt='{message}', style='{')
+    admin_bot_handler.setFormatter(botformatter)
+    logger.addHandler(admin_bot_handler)
+
+    streamhandler = logging.StreamHandler()
+    streamhandler.setLevel(logging.ERROR)
+    streamformatter = logging.Formatter(
+        fmt='{asctime} - {levelname} - {name} - {message}',
+        style='{')
+    streamhandler.setFormatter(streamformatter)
+    logger.addHandler(streamhandler)
+    logger.debug('VK бот запущен')
+
     try:
-        flow = DialogFlow(os.getenv('PROJECT_ID'))
-        flow_answer = flow.detect_intent_text(
-            user_id, user_message)
-        if not flow_answer['is_fallback']:
-            vk_api.messages.send(
-                user_id=user_id,
-                message=flow_answer['answer'],
-                random_id=random.randint(1, 1000000000)
-            )
-    except Exception:
-        logging.exception('Проблема с получением/отправкой сообщений')
-
-
-if __name__ == "__main__":
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.INFO)
-    logger.addHandler(LogsHandler())
-    logger.info('VK бот запущен')
-
-    try:
-        vk_session = vk.VkApi(token=os.getenv('VK_ACCESS_TOKEN'))
+        vk_session = vk.VkApi(token=os.environ.get('VK_COMMUNITY_TOKEN'))
         vk_api = vk_session.get_api()
         longpoll = VkLongPoll(vk_session)
+
         for event in longpoll.listen():
             if event.type == VkEventType.MESSAGE_NEW and event.to_me:
                 echo(event, vk_api)
-    except Exception:
-        logger.exception('Возникла ошибка в VK боте')
+
+    except Exception as e:
+        logger.debug('Возникла ошибка в DialogFlow vk-боте')
+        logger.exception(e)
